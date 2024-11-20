8950b1ba0997ab682fb6954c437afc15b81050cb
Huzaifa Ali <123181981+Huzaifa5125@users.noreply.github.com>
2024-11-20 19:48:23 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@/components': './src/components',
+      '@/lib': './src/lib',
+      '@/types': './src/types'
+    };
+    return config;
+  }
 };
 
 export default nextConfig;
